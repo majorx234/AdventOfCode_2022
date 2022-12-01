38fc07d08a1e0bd07f4067ddac5aa537e0a5f14a
MajorX234 <majorx234@googlemail.com>
2022-12-02 00:03:54 +0100

    Add unified solution for first and second Task of day1

--- day_1/src/bin/task1.rs
+++ day_1/src/bin/task1.rs
@@ -3,25 +3,30 @@ use std::io::{self, prelude::*, BufReader};
 
 fn main() {
     let reader = read_arg_file().unwrap();
-    let max_elfsum_id: (u32, u32, u32) = (0, 0, 0);
-    let fold_fct_task1 = |acc, x: Result<String, _>| {
+    let max123_elfsum_id: (u32, u32, u32, u32, u32) = (0, 0, 0, 0, 0);
+    let fold_fct_task = |acc, x: Result<String, _>| {
         if let Ok(x) = x {
-            let (max, elfsum, id) = acc;
+            let (max1, max2, max3, elfsum, id) = acc;
             if x == "" {
-                if max < elfsum {
-                    return (elfsum, 0, id + 1);
+                if max1 < elfsum {
+                    return (elfsum, max1, max2, 0, id + 1);
+                } else if max2 < elfsum {
+                    return (max1, elfsum, max2, 0, id + 1);
+                } else if max3 < elfsum {
+                    return (max1, max2, elfsum, 0, id + 1);
                 } else {
-                    return (max, 0, id + 1);
+                    return (max1, max2, max3, 0, id + 1);
                 }
             } else if let x = x.parse::<u32>().unwrap() {
-                return (max, elfsum + x, id);
+                return (max1, max2, max3, elfsum + x, id);
             } else {
-                return (max, elfsum, id);
+                return (max1, max2, max3, elfsum, id);
             }
         }
         acc
     };
-    let fourtytwo = reader.lines().fold(max_elfsum_id, fold_fct_task1);
-    let (max_elf, _, _) = fourtytwo;
-    println!("{}", max_elf);
+    let fourtytwo = reader.lines().fold(max123_elfsum_id, fold_fct_task);
+    let (max1, max2, max3, _, _) = fourtytwo;
+    println!("1st:{}", max1);
+    println!("2nd:{}", max1 + max2 + max3);
 }
