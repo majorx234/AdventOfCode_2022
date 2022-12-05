00366a0833158a3be4c9602940daf4a19a5aab96
Majorx234 <majorx234@googlemail.com>
2022-12-05 03:32:47 +0100

    Replace single character strings with char

--- day_4/src/bin/task2.rs
+++ day_4/src/bin/task2.rs
@@ -6,9 +6,9 @@ fn main() {
     let sum: u32 = 0;
     let fold_fct_task = |acc, x: Result<String, _>| {
         if let Ok(x) = x {
-            let split: Vec<&str> = x.split(",").collect();
-            let elf1: Vec<&str> = split[0].split("-").collect();
-            let elf2: Vec<&str> = split[1].split("-").collect();
+            let split: Vec<&str> = x.split(',').collect();
+            let elf1: Vec<&str> = split[0].split('-').collect();
+            let elf2: Vec<&str> = split[1].split('-').collect();
             let elf1_low_bound = elf1[0].to_string().parse::<u32>().unwrap();
             let elf1_high_bound = elf1[1].to_string().parse::<u32>().unwrap();
             let elf2_low_bound = elf2[0].to_string().parse::<u32>().unwrap();
