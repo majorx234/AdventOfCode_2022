16da3e45b5154a555a32331e3a5d61f4827fc8ea
Majorx234 <majorx234@googlemail.com>
2022-12-20 02:40:46 +0100

    Add function to generate cave model

--- day_14/src/bin/task1.rs
+++ day_14/src/bin/task1.rs
@@ -9,8 +9,29 @@ use std::{fs::read_to_string, path::Path};
 
 #[derive(Debug)]
 struct RockStruct {
-    id: u32,
     points: Vec<(i32, i32)>,
+}
+
+#[derive(Debug)]
+struct Cave {
+    offset_x: i32,
+    offset_y: i32,
+    tiles: Vec<Vec<bool>>,
+}
+
+impl Cave {
+    fn print(&self) {
+        for y in 0..self.tiles.len() {
+            for x in 0..self.tiles[0].len() {
+                if self.tiles[y][x] == true {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    }
 }
 
 fn parse_point(input: &str) -> IResult<&str, (i32, i32), ()> {
@@ -21,18 +42,78 @@ fn parse_point(input: &str) -> IResult<&str, (i32, i32), ()> {
 }
 fn parse_rockstruct(input: &str) -> IResult<&str, RockStruct, ()> {
     let (input, points) = separated_list1(tag(" -> "), parse_point)(input)?;
-    Ok((
-        input,
-        RockStruct {
-            id: 0,
-            points: points,
-        },
-    ))
+    Ok((input, RockStruct { points: points }))
 }
 
 fn parse_input(input: &str) -> IResult<&str, Vec<RockStruct>, ()> {
     let (input, lines) = separated_list1(newline, parse_rockstruct)(input)?;
     Ok((input, lines))
+}
+
+fn interpolate(start: (i32, i32), end: (i32, i32)) -> Vec<(i32, i32)> {
+    let (start_x, start_y) = start;
+    let (end_x, end_y) = end;
+    let min_x = start_x.min(end_x);
+    let max_x = start_x.max(end_x);
+    let min_y = start_y.min(end_y);
+    let max_y = start_y.max(end_y);
+    let delta_x = start_x.abs_diff(end_x) as usize;
+    let delta_y = start_y.abs_diff(end_y) as usize;
+    if delta_x != 0 {
+        (min_x..(max_x + 1)).zip(vec![min_y; delta_x + 1]).collect()
+    } else {
+        vec![min_x; delta_y + 1]
+            .into_iter()
+            .zip(min_y..(max_y + 1))
+            .collect()
+    }
+}
+
+fn generate_cave_model(rockstructs: Vec<RockStruct>) -> Option<Cave> {
+    // find min_x, max_x, min_y, max_y
+    let (mut min_x, mut min_y) = rockstructs[0].points[0];
+    let (mut max_x, mut max_y) = rockstructs[0].points[0];
+    for rockstruct in &rockstructs {
+        for (x, y) in &rockstruct.points {
+            if *x > max_x {
+                max_x = *x;
+            }
+            if *y > max_y {
+                max_y = *y;
+            }
+            if *x < min_x {
+                min_x = *x;
+            }
+            if *y < min_y {
+                min_y = *y;
+            }
+        }
+    }
+
+    let size_x = ((max_x - min_x) + 1) as usize;
+    let size_y = ((max_y - min_y) + 1) as usize;
+    let offset_x = min_x;
+    let offset_y = min_y;
+    let mut cave_model = Cave {
+        offset_x: offset_x,
+        offset_y: offset_y,
+        tiles: vec![vec![false; size_x]; size_y],
+    };
+    for rockstruct in rockstructs {
+        for index in 0..(rockstruct.points.len() - 1) {
+            let start = rockstruct.points[index];
+            let end = rockstruct.points[index + 1];
+
+            let rock_tiles = interpolate(start, end);
+
+            for (x, y) in rock_tiles {
+                cave_model.tiles[(y - offset_y) as usize][(x - offset_x) as usize] = true;
+            }
+        }
+    }
+    cave_model.print();
+
+    Some(cave_model)
 }
 
 fn main() {
@@ -46,5 +127,5 @@ fn main() {
         panic!("No filename argument given");
     };
     let (_, mut rockstructs) = parse_input(&input).unwrap();
-    println!("{:?}", rockstructs);
+    generate_cave_model(rockstructs);
 }
